e23ea324d8847b04d5cf20f3d5334b74b603b085
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-06-29 18:15:35 -0500

    Added base, local and prod settings

---
+++ back/portfolio/portfolio/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
