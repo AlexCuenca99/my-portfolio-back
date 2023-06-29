101b10e0fe85a14e903142fc1d87557c98c1bcc9
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-06-29 18:27:59 -0500

    Added environment variables

--- back/portfolio/portfolio/settings/base.py
+++ back/portfolio/portfolio/settings/base.py
@@ -1,15 +1,24 @@
 from pathlib import Path
+import environ
 import os
+
+env = environ.Env()
+env.prefix = "DJANGO_"
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Take environment variables from .env file
+
+environ.Env.read_env(os.path.join(BASE_DIR, os.pardir, ".env"))
+
+
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z4z)c!5p91=0-+!t_5x5q2sy!f-ok%*8or7486fl#m223y%it!'
+SECRET_KEY = env('SECRET_KEY')
 
 
 # Application definition
--- back/portfolio/portfolio/settings/local.py
+++ back/portfolio/portfolio/settings/local.py
@@ -11,7 +11,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('PSQL_NAME'),
+        'USER': env('PSQL_USER'),
+        'PASSWORD': env('PSQL_PASSWORD'),
+        'HOST': env('PSQL_HOST'),
+        'PORT': env('PSQL_PORT'),
     }
 }
