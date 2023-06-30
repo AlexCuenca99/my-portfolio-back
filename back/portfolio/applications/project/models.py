42da29817279e1ad2f8f4ab8899e3d5cc2e78fad
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-06-29 20:02:27 -0500

    Added Proejct Model

--- back/portfolio/applications/project/models.py
+++ back/portfolio/applications/project/models.py
@@ -1,3 +1,48 @@
+import os
 from django.db import models
 
-# Create your models here.
+from multiselectfield import MultiSelectField
+from model_utils.models import TimeStampedModel
+
+
+PYT = "PYTHON"
+NEX = "NEXTJS"
+DJA = "DJANGO"
+REA = "REACTJS"
+PSQ = "POSTGRESQL"
+AWS = "AMAZONWEBSERVICES"
+DRF = "DJANGORESTFRAMEWORK"
+
+TECHS_CHOICES = (
+    (PYT, "Python"),
+    (NEX, "Next JS"),
+    (REA, "React JS"),
+    (PSQ, "PostgreSQL"),
+    (AWS, "Amazon Web Services"),
+    (DJA, "Django Web Framework"),
+    (DRF, "Django Rest Framework"),
+)
+
+
+def screenshot_file_name(self, filename):
+    extension = filename.split(".")[-1]
+    filename = "screenshot_{}.{}".format(self.id, extension)
+    return os.path.join("portfolio/media/projects/photos/screenshots", filename)
+
+
+class Project(TimeStampedModel):
+    url = models.URLField("Website", max_length=200)
+    title = models.CharField("Project Title", max_length=30)
+    description = models.TextField("Description", max_length=100)
+    in_production = models.BooleanField("Is in production", default=False)
+    screenshot = models.ImageField(
+        "Screenshot", upload_to=screenshot_file_name, blank=True, null=True
+    )
+    technologies = MultiSelectField(choices=TECHS_CHOICES, max_length=20, max_choices=6)
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return f"{self.title} en {self.url}"
--- back/portfolio/portfolio/settings/base.py
+++ back/portfolio/portfolio/settings/base.py
@@ -18,75 +18,75 @@ environ.Env.read_env(os.path.join(BASE_DIR, os.pardir, ".env"))
 # Quick-start development settings - unsuitable for production
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 
 # Application definition
 
 DJANGO_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 )
 
 LOCAL_APPS = (
-    'applications.article',
-    'applications.project',
-    'applications.home',
+    "applications.article",
+    "applications.project",
+    "applications.home",
 )
 
-THIRD_PARTY_APPS = ()
+THIRD_PARTY_APPS = ("multiselectfield",)
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = "portfolio.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = "portfolio.wsgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -94,9 +94,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -107,8 +107,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "uploads")
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 
@@ -116,4 +116,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 
 # Default primary key field type
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
