71ca51b63565e799d235216aada250fcce01435e
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-01 14:23:20 -0500

    Added local settings

--- back/portfolio/portfolio/settings/local.py
+++ back/portfolio/portfolio/settings/local.py
@@ -4,18 +4,26 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('PSQL_NAME'),
-        'USER': env('PSQL_USER'),
-        'PASSWORD': env('PSQL_PASSWORD'),
-        'HOST': env('PSQL_HOST'),
-        'PORT': env('PSQL_PORT'),
+if env("LOCAL_DEV_PHASE") == "data_modeling":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+elif env("LOCAL_DEV_PHASE") == "develop":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": env("PSQL_NAME"),
+            "USER": env("PSQL_USER"),
+            "PASSWORD": env("PSQL_PASSWORD"),
+            "HOST": env("PSQL_HOST"),
+            "PORT": env("PSQL_PORT"),
+        }
+    }
