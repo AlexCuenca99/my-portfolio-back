504c86c21c0732e4ee891d60b1ec347d4272d8cb
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-01 14:55:10 -0500

    App Home modeling

--- back/portfolio/applications/home/models.py
+++ back/portfolio/applications/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class Home(TimeStampedModel):
+    current_job = models.CharField("Current job", max_length=75)
+    creator_name = models.CharField("Creator name", max_length=50)
+    self_description = models.TextField("Self escription", max_length=125)
+    short_self_description = models.TextField("Short self description", max_length=60)
+
+    class Meta:
+        verbose_name = "Home"
+        verbose_name_plural = "Home"
+
+    def __str__(self):
+        return f"{self.creator_name}"
