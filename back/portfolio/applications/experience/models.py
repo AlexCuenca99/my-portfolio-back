84f28d79a38ee55bb7485bd572e987e2cf9f4fe7
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-01 14:43:28 -0500

    Added Experience Attributes

--- back/portfolio/applications/experience/models.py
+++ back/portfolio/applications/experience/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from multiselectfield import MultiSelectField
+from model_utils.models import TimeStampedModel
+
+from .choices import SKILLS_CHOICES
+
+
+class Experience(TimeStampedModel):
+    role = models.CharField("Job role", max_length=100)
+    end_date = models.PositiveSmallIntegerField("End date")
+    start_date = models.PositiveSmallIntegerField("Start date")
+    role_description = models.TextField("Role description", max_length=125)
+    skills = MultiSelectField(
+        "Role skills",
+        choices=SKILLS_CHOICES,
+        max_length=30,
+        max_choices=4,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Experience"
+        verbose_name_plural = "Experiences"
+
+    def __str__(self):
+        return f"{self.role} since {self.start_date} to {self.end_date}"
