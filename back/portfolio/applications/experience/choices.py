98f69b978d85008a89d30b0d3643ac6a58d2633e
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-01 14:43:06 -0500

    Added skills choices

---
+++ back/portfolio/applications/experience/choices.py
@@ -0,0 +1,18 @@
+# Skills choices
+PYT = "PYTHON"
+NEX = "NEXTJS"
+DJA = "DJANGO"
+REA = "REACTJS"
+PSQ = "POSTGRESQL"
+AWS = "AMAZONWEBSERVICES"
+DRF = "DJANGORESTFRAMEWORK"
+
+SKILLS_CHOICES = (
+    (PYT, "Python"),
+    (NEX, "Next JS"),
+    (REA, "React JS"),
+    (PSQ, "PostgreSQL"),
+    (AWS, "Amazon Web Services"),
+    (DJA, "Django Web Framework"),
+    (DRF, "Django Rest Framework"),
+)
